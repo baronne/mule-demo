9854e1e84708dbd4f055a32e749bbc64f509c078
baronne <baronne@mouton.co.uk>
2022-02-26 19:45:22 +0000

    added variables into config

--- variables.tf
+++ variables.tf
@@ -7,6 +7,18 @@ variable "aws_profile" {
   type = string
 }
 
-variable "mysql_password" {
+variable "instance_type" {
+  type = string
+}
+
+variable "rds_instance_class" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
   type = string
 }
