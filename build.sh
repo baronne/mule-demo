d49cde7181743ea8fb8123755cd24edb97a1b8ba
baronne <baronne@mouton.co.uk>
2022-02-26 19:38:23 +0000

    added quick build and reset shortcut commands

---
+++ build.sh
@@ -0,0 +1,1 @@
+time terraform apply --auto-approve -target=module.vpc && terraform apply --auto-approve
---
+++ reset.sh
@@ -0,0 +1,1 @@
+time terraform destroy --auto-approve && terraform apply --auto-approve -target=module.vpc && terraform apply --auto-approve
