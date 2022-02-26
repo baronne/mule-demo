55704ec3f68206c0a58cf7f29c8e19057dc48979
baronne <baronne@mouton.co.uk>
2022-02-26 19:45:07 +0000

    modified for change to ubuntu

--- userdata.sh
+++ userdata.sh
@@ -1,10 +1,14 @@
-#!/bin/bash -x
-# set -e
+#!/bin/bash
 
-# Ouput all logs
-# exec > >(tee /var/log/user-data.log|logger -t user-data-extra -s 2>/dev/console) 2>&1
+Ouput all logs
+exec > >(tee /var/log/user-data.log|logger -t user-data-extra -s 2>/dev/console) 2>&1
 
-#yum update
+# Amazon Linux:
+# sudo amazon-linux-extras install epel
 # sudo yum update -y
-# sudo yum upgrade -y
+# sudo yum install ansible -y
+# sudo ansible-galaxy collection install community.general
 
+sudo apt update -y
+sudo apt install ansible -y
+sudo ansible-galaxy collection install community.general
