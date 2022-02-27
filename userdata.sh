880f32f0c5c600ece037e43a22477767b6960ca7
baronne <baronne@mouton.co.uk>
2022-02-27 11:46:45 +0000

    install latest ansible version 2.12+ and other dependencies

--- userdata.sh
+++ userdata.sh
@@ -7,8 +7,12 @@ exec > >(tee /var/log/user-data.log|logger -t user-data-extra -s 2>/dev/console)
 # sudo amazon-linux-extras install epel
 # sudo yum update -y
 # sudo yum install ansible -y
+# sudo yum install unzip -y
 # sudo ansible-galaxy collection install community.general
 
 sudo apt update -y
+sudo apt install software-properties-common -y
+sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
-sudo ansible-galaxy collection install community.general
+sudo apt install unzip -y
+# sudo ansible-galaxy collection install community.general
