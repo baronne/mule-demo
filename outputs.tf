38ca08ce190dca7f15ebcf19fc5506074f4a7aad
baronne <baronne@mouton.co.uk>
2022-02-28 16:44:19 +0000

    added api console url output

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,11 @@ output "apihost_public_ip" {
 #   value = module.db.db_instance_password
 #   sensitive = true
 # }
+
+output "rds_endpoint" {
+  value = module.rds_mysql[*].db_instance_address
+}
+
+output "api_console_url" {
+  value = "http://${module.api_host[0].public_ip}:8081/console"
+}
