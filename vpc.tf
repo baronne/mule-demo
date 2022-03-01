c03f32297add698c0a9376f6d71bd71349eaeccb
baronne <baronne@mouton.co.uk>
2022-03-01 12:32:58 +0000

    disabled nat gateway

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,7 @@ module "vpc" {
   private_subnets = ["10.${count.index + 1}.1.0/24", "10.${count.index + 1}.2.0/24"]
   public_subnets  = ["10.${count.index + 1}.101.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = false
   single_nat_gateway = true
 
   enable_dns_hostnames = true
